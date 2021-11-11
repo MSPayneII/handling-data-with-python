38ee972f04c35929ded3ed7eca6ebb5a2c9e3735
Michael Payne <mpaynei@umich.edu>
2021-11-11 14:18:09 -0500

    updating hw

---
+++ data_presenter.py
@@ -0,0 +1,37 @@
+open_file = open('CupcakeInvoices.csv')
+
+for line in open_file:
+    print(line)
+
+open_file.close()
+
+open_file = open('CupcakeInvoices.csv')
+
+for line in open_file:
+    line_words = line.rstrip('\n').split(',')
+    print(line_words[2])
+
+open_file.close()
+
+
+open_file = open('CupcakeInvoices.csv')
+
+for line in open_file:
+    line_words = line.rstrip('\n').split(',')
+
+    quantity = int(line_words[3])
+    price = float(line_words[4])
+
+    total_before_rounding = quantity * price
+    final_total = round(total_before_rounding,2)
+    print(final_total)
+
+open_file.close()
+
+# open_file = open('CupcakeInvoices.csv')
+
+# for line in open_file:
+#     line_words = line.split(',')
+#     print(line_words)
+
+# open_file.close()
